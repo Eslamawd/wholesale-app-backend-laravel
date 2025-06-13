31618176547d6219f33f81713f8fbf012761e0a6
eslamawd <eeslamawood@gmail.com>
2025-06-13 12:03:34 +0300

    run Laravel on Railway

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [ 'https://peaceful-strength-production.up.railway.app' ,],
+    'allowed_origins' => [ 'https://wholesale-front-react-production.up.railway.app' ,],
 
     'allowed_origins_patterns' => [],
 
