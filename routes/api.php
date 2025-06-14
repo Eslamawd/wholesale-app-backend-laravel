f4c665fa7d1037bcc68900ab4dd2af0b71778a61
eslamawd <eeslamawood@gmail.com>
2025-06-14 18:25:34 +0300

    run Laravel on Railway

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/orders', [OrderController::class, 'store']);
     Route::get('/orders', [OrderController::class, 'index']);
     Route::get('/wallet/balance', [WalletController::class, 'balance']);
+
     
     Route::get('/services', [ServiceController::class, 'index']);
     Route::get('/services/{id}', [ServiceController::class, 'show']);
